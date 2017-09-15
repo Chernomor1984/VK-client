6d4e370e5c400bd83bddfcba37203549e901fefd
Eugene Khizhnyak <eugene.khizhnyak@rcntec.com>
2017-09-15 18:44:44 +0300

    login button tap handler added

--- Weather/LoginFormController.swift
+++ Weather/LoginFormController.swift
@@ -22,6 +22,35 @@ class LoginFormController: UIViewController {
         addHideKeyboradGestureRecognizer()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(KeyboardWillHide(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func loginButtonTapHandler(_ sender: UIButton) {
+        let login = loginTextField!.text
+        let password = passwordTextField!.text
+        
+        if login == "admin" && password == "admin" {
+            print("авторизация успешна!")
+        } else {
+            print("данные неверны")
+        }
+    }
+    
     // MARK: Private
     
     func addHideKeyboradGestureRecognizer() {
@@ -31,5 +60,20 @@ class LoginFormController: UIViewController {
     
     @objc func hideKeyboard() {
         scrollView?.endEditing(true)
+    }
+    
+    // MARK: Notifications
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let keyboardSize = (info.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInset = UIEdgeInsetsMake(0, 0, keyboardSize.height, 0)
+        scrollView?.contentInset = contentInset;
+        scrollView?.scrollIndicatorInsets = contentInset
+    }
+    
+    @objc func KeyboardWillHide(notification: Notification) {
+        scrollView?.contentInset = UIEdgeInsets.zero
+        scrollView?.scrollIndicatorInsets = UIEdgeInsets.zero
     }
 }
