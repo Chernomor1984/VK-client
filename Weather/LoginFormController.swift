044db39aba07216455f15feec8a65177d7782966
Eugene Khizhnyak <eugene.khizhnyak@rcntec.com>
2017-09-15 17:05:51 +0300

    Initial Commit

---
+++ Weather/LoginFormController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginFormController.swift
+//  Weather
+//
+//  Created by Eugene Khizhnyak on 12.09.17.
+//  Copyright © 2017 RCNTEC. All rights reserved.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginButtonTapHandler(_ sender: UIButton) {
+    }
+}
