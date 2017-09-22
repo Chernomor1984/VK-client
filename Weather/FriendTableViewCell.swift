75bf2b2a490cf71b234fa1a7aaa5245254e91967
Eugene Khizhnyak <eugene.khizhnyak@rcntec.com>
2017-09-22 16:47:46 +0300

    Avatar + name + test icons added

---
+++ Weather/FriendTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FriendTableViewCell.swift
+//  Weather
+//
+//  Created by Eugene Khizhnyak on 22.09.17.
+//  Copyright © 2017 RCNTEC. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
--- Weather/FriendsListController.swift
+++ Weather/FriendsListController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class TableViewController1: UITableViewController {
+class FriendsListController: UITableViewController {
     
-    let items = ["cell1", "cell2", "cell3"]
+    let itemsDictionary = ["avatar1" : "John Smith", "avatar2" : "Victoria Graham", "avatar3" : "Matt Busby"]
     
     // MARK: - Life cycle
     
@@ -25,13 +25,16 @@ class TableViewController1: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return itemsDictionary.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        cell.textLabel?.text = items[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! FriendTableViewCell
+        let keys = Array(itemsDictionary.keys)
+        let key = keys[indexPath.row]
+        cell.nameLabel.text = itemsDictionary[key]
+        cell.avatarImageView.image = UIImage(named:key)
         return cell
     }
     
