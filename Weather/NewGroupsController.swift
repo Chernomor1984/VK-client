c5968ed3d40380cafcb18038227b78bfb5e25d56
Eugene Khizhnyak <eugene.khizhnyak@rcntec.com>
2017-09-22 17:40:46 +0300

    WIP: NewGroupsController

---
+++ Weather/NewGroupsController.swift
@@ -0,0 +1,33 @@
+//
+//  NewGroupsTableViewController.swift
+//  Weather
+//
+//  Created by Eugene Khizhnyak on 22.09.17.
+//  Copyright © 2017 RCNTEC. All rights reserved.
+//
+
+import UIKit
+
+class NewGroupsController: UITableViewController {
+
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        return cell
+    }
+}
