f17af09f67547ff8b713faeff524da9b59a88bdb
Eugene Khizhnyak <eugene.khizhnyak@rcntec.com>
2017-10-31 02:36:04 +0300

    ImageToRowBindingOperation added

---
+++ Weather/ImageToRowBindingOperation.swift
@@ -0,0 +1,65 @@
+//
+//  ImageToRowBindingOperation.swift
+//  VK
+//
+//  Created by Eugene Khizhnyak on 31.10.17.
+//  Copyright © 2017 RCNTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageToRowBindingOperation: Operation {
+    private let indexPath: IndexPath
+    private let container: CellDataUpdating
+    
+    // MARK: - Init
+    
+    init(indexPath: IndexPath, container: UITableView) {
+        self.indexPath = indexPath
+        self.container = TableView(container)
+    }
+    
+    init(indexPath: IndexPath, container: UICollectionView) {
+        self.indexPath = indexPath
+        self.container = CollectionView(container)
+    }
+    
+    // MARK: - Main
+    
+    override func main() {
+        guard let cachedImageOperation = dependencies[0] as? CachedImageOperation,
+            let image = cachedImageOperation.outputImage else {
+                return
+        }
+        container.updateCellImage(image: image, atIndexPath: indexPath)
+    }
+}
+
+fileprivate protocol CellDataUpdating {
+    func updateCellImage(image: UIImage, atIndexPath indexPath: IndexPath)
+}
+
+extension ImageToRowBindingOperation {
+    private class TableView: CellDataUpdating {
+        let tableView: UITableView
+        
+        init(_ tableView: UITableView) {
+            self.tableView = tableView
+        }
+        
+        func updateCellImage(image: UIImage, atIndexPath indexPath: IndexPath) {
+        }
+    }
+    
+    private class CollectionView: CellDataUpdating {
+        let collectionView: UICollectionView
+        
+        init(_ collectionView: UICollectionView) {
+            self.collectionView = collectionView
+        }
+        
+        func updateCellImage(image: UIImage, atIndexPath indexPath: IndexPath) {
+        }
+    }
+}
---
+++ Weather/PhotoDownloadService.swift
@@ -0,0 +1,11 @@
+//
+//  PhotoDownloadService.swift
+//  VK
+//
+//  Created by Eugene Khizhnyak on 31.10.17.
+//  Copyright © 2017 RCNTEC. All rights reserved.
+//
+
+import Foundation
+
+class PhotoDownloadService {}
