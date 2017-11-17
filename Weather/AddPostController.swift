3dec48f0f7b7314062038b2cf15103c00925b108
Eugene Khizhnyak <eugene.khizhnyak@rcntec.com>
2017-11-17 17:29:26 +0300

    Map view added

--- Weather/AddPostController.swift
+++ Weather/AddPostController.swift
@@ -8,15 +8,50 @@
 
 import UIKit
 import Foundation
+import GoogleMaps
 
 class AddPostController: UIViewController {
-    
     @IBOutlet var textView: UITextView!
+    
+    let minZoom: Float = 6.0
+    let maxZoom: Float = 19.0
+    
+    private lazy var camera: GMSCameraPosition = {
+        let coordinate = CLLocationCoordinate2D()
+        let cm = GMSCameraPosition.camera(withTarget: coordinate, zoom: minZoom)
+        return cm
+    }()
+    
+    private lazy var mapView: GMSMapView = {
+        let mv = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mv.isMyLocationEnabled = true
+        return mv
+    }()
     
     // MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.automaticallyAdjustsScrollViewInsets = false
+        automaticallyAdjustsScrollViewInsets = false
+        self.view.addSubview(mapView)
+        configureMapViewConstraints()
+    }
+    
+    // MARK: - Private
+    
+    private func configureMapViewConstraints() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        if #available(iOS 11, *) {
+            let guide = view.safeAreaLayoutGuide
+            NSLayoutConstraint.activate([guide.bottomAnchor.constraintEqualToSystemSpacingBelow(mapView.bottomAnchor, multiplier: 1.0)])
+        } else {
+            let standardSpacing: CGFloat = 0.0
+            NSLayoutConstraint.activate([bottomLayoutGuide.topAnchor.constraint(equalTo: mapView.bottomAnchor, constant: standardSpacing)])
+        }
+        let top = NSLayoutConstraint(item: textView, attribute: .bottom, relatedBy: .equal, toItem: mapView, attribute: .top, multiplier: 1.0, constant: 0.0);
+        let leading = NSLayoutConstraint(item: self.view, attribute: .leading, relatedBy: .equal, toItem: mapView, attribute: .leading, multiplier: 1.0, constant: 0.0)
+        let trailing = NSLayoutConstraint(item: mapView, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: 0.0)
+        view.addConstraints([top, leading, trailing])
     }
 }
