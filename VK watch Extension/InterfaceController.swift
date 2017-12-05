f5ee5c06bf9b7ac89357ed36cc548c077496c125
Eugene Khizhnyak <eugene.khizhnyak@rcntec.com>
2017-12-05 17:43:06 +0300

    WIP: Watch OS

---
+++ VK watch Extension/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  VK watch Extension
+//
+//  Created by Eugene Khizhnyak on 05.12.17.
+//  Copyright © 2017 RCNTEC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+    }
+    
+    override func willActivate() {
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        super.didDeactivate()
+    }
+
+}
